81b90e6b1a54d7c090ddcafe412eb8a3aece056b
Jess Long <jessicaemilylong@hotmail.co.uk>
2026-05-18 16:09:01 +0100

    add features to product calls

--- backend/app/services/product_service.py
+++ backend/app/services/product_service.py
@@ -72,8 +72,90 @@ def get_product_budget_ranges(query: str):
     price_ranges = extract_filter_by_type("price_range", filters)
     return price_ranges 
 
+def format_product(item: dict, min_price: float, max_price: float):
+    price = item.get("extracted_price")
+    if price is None:
+        return None
+    if price < min_price or price > max_price:
+        return None
+
+    title = item.get("title")
+    if title is None:
+        return None
+
+    brand = item.get("source", "Online retailer")
+    if "Amazon" in brand:
+        brand = "Amazon"
+
+    # Simple product quality score
+    rating = item.get("rating", 0)
+    reviews = item.get("reviews", 0)
+    additional_features = item.get("extensions", [])
+    score = (
+        rating * 10 +
+        min(reviews, 200) +
+        len(additional_features)
+    )
+
+    return {
+        "name": title,
+        "brand": brand,
+        "rating": rating,
+        "image": item.get("thumbnail", ""),
+        "features": [
+            feature for feature in [
+                item.get("delivery"),
+                item.get("snippet"),
+                item.get("snippet_highlighted_words"),
+                item.get("tagline")
+            ]
+            if feature
+        ],
+        "additionalFeatures": item.get("extensions", []),
+        "webUrl": item.get("product_link", ""),
+        "price": price,
+        "PRODUCT_PAGE_TOKEN": item.get("immersive_product_page_token", ""),
+        "RANKING_SCORE": score
+    }
+
+def extract_immersive_features(page_token: str):
+    if not page_token:
+        return []
+    results = serpapi_client.search({
+        "engine": "google_immersive_product",
+        "page_token": page_token
+    })
+
+    feature_data = (
+        results
+            .get("product_results", {})
+            .get("about_the_product", {})
+            .get("features", [])
+    )
+
+    extracted_features = []
+
+    for feature in feature_data:
+        title = feature.get("title")
+        value = feature.get("value")
+        
+        if value.lower() in ["no", "false", "none"]:
+            continue
+        if value.lower() in ["yes", "true"]:
+            extracted_features.append(title)
+            continue
+        if value.replace(".", "", 1).isdigit():
+            extracted_features.append(f"{title}: {value}")
+            continue
+        if "," in value:
+            values = [part.strip() for part in value.split(",") if part.strip()]
+            extracted_features.extend(values)
+            continue
+        extracted_features.append(value)
+
+    return extracted_features
+
 def get_product_list(query: str, min_price: float, max_price: float):
-    print("query", query)
     results = serpapi_client.search({
       "engine": "google_shopping_light",
       "google_domain": "google.co.uk",
@@ -86,55 +168,19 @@ def get_product_list(query: str, min_price: float, max_price: float):
 
     filtered_products = []
     for item in shopping_results:
-        price = item.get("extracted_price")
+        product = format_product(item, min_price, max_price)
+        if product:
+            filtered_products.append(product)
 
-        if price is None:
-            continue
-        if price < min_price or price > max_price:
-            continue
+    filtered_products.sort(
+        key=lambda product: product["RANKING_SCORE"],
+        reverse=True
+    )
+    top_products = filtered_products[:3]
+    # Top 3 Products get further information
 
-        title = item.get("title")
-        if title is None:
-            continue
+    for item in top_products:
+        immersive_features = extract_immersive_features(item["PRODUCT_PAGE_TOKEN"])
+        item["additionalFeatures"].extend(immersive_features)
 
-        brand = item.get("source", "Online retailer")
-        if "Amazon" in brand:
-            brand = "Amazon"
-
-        rating = item.get("rating", 0)
-        reviews = item.get("reviews", 0)
-        additional_features = item.get("extensions", [])
-
-        # Simple product quality score
-        score = (
-            rating * 10 +
-            min(reviews, 200) +
-            len(additional_features)
-        )
-
-        filtered_products.append({
-            "name": title,
-            "brand": brand,
-            "rating": rating,
-            "image": item.get("thumbnail", ""),
-            "features": [
-                feature for feature in [
-                    item.get("delivery"),
-                    item.get("snippet"),
-                    item.get("snippet_highlighted_words"),
-                    item.get("tagline")
-                ]
-                if feature
-            ],
-            "additionalFeatures": item.get("extensions", []),
-            "webUrl": item.get("product_link", ""),
-            "price": price,
-            "RANKING_SCORE": score
-        })
-
-        filtered_products.sort(
-            key=lambda product: product["RANKING_SCORE"],
-            reverse=True
-        )
-        
-    return filtered_products[:5]
+    return top_products
