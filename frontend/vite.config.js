64566e2a695f76e53871baf209e7b8538de98548
Jess Long <146840057+JesssseL@users.noreply.github.com>
2026-06-09 01:48:16 +0100

    Update vite.config.js to add azure as permitted host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    //vueDevTools(),
-  ],
+  plugins: [vue()],
+
+  preview: {
+    host: '0.0.0.0',
+    port: 8080,
+    allowedHosts: [
+      'dissfrontend-efcvgme6e4e4agav.germanywestcentral-01.azurewebsites.net'
+    ]
+  },
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    }
+  }
 })
